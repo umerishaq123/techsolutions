f42dd2ddfebded6f182453bdee5acffd26fe8444
umerishaq123 <umerishaq7387@gmail.com>
2024-08-05 16:13:44 +0500

    first commit

---
+++ lib/map/googlemap.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoogleMap extends StatefulWidget {
+  const GoogleMap({super.key});
+
+  @override
+  State<GoogleMap> createState() => _GoogleMapState();
+}
+
+class _GoogleMapState extends State<GoogleMap> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
